84a652d7963d9078a551a6dcb338a9be43fb6058
Dinesh Kumar <dinesh.kumar@go-jek.com>
2017-04-09 00:55:21 +0530

    Added Travis ci for tests, and simplecov for tests
    commit 1cc0f6a9f2bc98b6cc6437ead47b4ed979636719
    Author: Dinesh Kumar <dinesh.kumar@go-jek.com>
    Date:   Sun Apr 9 00:44:40 2017 +0530
    
        Adding simplecov
    
    commit cda59a176ebf3a8bcfa15dd7173474ab7bb2f87c
    Author: Dinesh Kumar <dinesh.kumar@go-jek.com>
    Date:   Sun Apr 9 00:02:36 2017 +0530
    
        updated readme
    
    commit f0939c31b22b27592e8a0c1af92c69fa45bc0e6f
    Author: Dinesh Kumar <dinesh.kumar@go-jek.com>
    Date:   Sat Apr 8 23:53:07 2017 +0530
    
        Adding travis file

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,7 @@
+require 'simplecov'
+SimpleCov.start
+SimpleCov.minimum_coverage 88
+
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'generator_spec'
 require 'client_authenticator'
