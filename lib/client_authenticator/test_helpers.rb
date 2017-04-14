be9bbe2343b84daee9d72c7c5e5754234e1ceb82
Dinesh Kumar <dinesh.kumar@go-jek.com>
2017-04-14 20:56:56 +0530

    fixed method name, updated version for new gem release

--- lib/client_authenticator/test_helpers.rb
+++ lib/client_authenticator/test_helpers.rb
@@ -3,13 +3,13 @@ require 'client_authenticator'
 module ClientAuthenticator
   module TestHelpers
     def stub_valid_client_credentials
-      expect(ClientAuthenticator::ApiClient).to receive(:authenticate_client!).with('client_id', 'valid_pass_key').and_return(true)
+      expect(ClientAuthenticator::ApiClient).to receive(:authenticated?).with('client_id', 'valid_pass_key').and_return(true)
       @request.headers['client-id'] = 'client_id'
       @request.headers['pass-key'] = 'valid_pass_key'
     end
 
     def stub_invalid_client_credentials
-      expect(ClientAuthenticator::ApiClient).to receive(:authenticate_client!).with('client_id', 'invalid_pass_key').and_return(false)
+      expect(ClientAuthenticator::ApiClient).to receive(:authenticated?).with('client_id', 'invalid_pass_key').and_return(false)
       @request.headers['client-id'] = 'client_id'
       @request.headers['pass-key'] = 'invalid_pass_key'
     end
